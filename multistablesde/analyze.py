aa450d7b3b0445d70e5238eb4976c7648a47e4b2
Linus Heck <linus.heck@rwth-aachen.de>
2023-08-14 18:42:21 +0200

    Marginals in analysis script

--- multistablesde/analyze.py
+++ multistablesde/analyze.py
@@ -17,15 +17,27 @@ import torchsde
 
 from latent_sde import *
 
+def draw_marginals(xs_sde, xs_data, file):
+    bins = np.linspace(-4, 4, 100)
+    plt.hist(torch.flatten(xs_sde).numpy(), bins=bins, alpha=0.5, label="Latent SDE", edgecolor='black', linewidth=1.2)
+    plt.hist(torch.flatten(xs_data).numpy(), bins=bins, alpha=0.5, label="Data", edgecolor='black', linewidth=1.2)
+    plt.savefig(file)
+    plt.close()
+
 """
 The analysis script, WIP
 """
-def main(model = None, data = None):
-    latent_sde = torch.load(model)
-    tsxs = torch.load(data)
-    print(latent_sde)
-    print(tsxs)
-    print(latent_sde.sample(10, tsxs["ts"]))
+def main(model = None, data = None, out = None):
+    latent_sde = torch.load(model, map_location=torch.device("cpu"))
+    tsxs_data = torch.load(data, map_location=torch.device("cpu"))
+    
+    ts = tsxs_data["ts"]
+    
+    marginal_size = 1000
+    xs_sde = latent_sde.sample(marginal_size, ts)
+    xs_data = tsxs_data["xs"]
+    
+    draw_marginals(xs_sde, xs_data, f"{out}/marginals.pdf")
 
 if __name__ == "__main__":
     print(" ".join(sys.argv))
