cfc3822ce104f43a27221a319711de79ddbe2421
Linus Heck <linus.heck@rwth-aachen.de>
2023-08-19 10:35:54 +0200

    add scalar diffusion benchmark

---
+++ slurm/energybalance/betas_scalar_diffusion.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+fixed_args="multistablesde/latent_sde.py --latent-size=1 --lr_gamma=0.9995 --model=energy --scalar-diffusion=True"
+
+variable_args=(
+	"--beta=1"
+	"--beta=10"
+	"--beta=100"
+	"--beta=1000"
+	"--beta=10000"
+)
+
+srun python3 -m pipenv run python $fixed_args ${variable_args[$SLURM_ARRAY_TASK_ID]} --train-dir $1
+
