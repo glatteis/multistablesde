931e5b082fbc6626c000459a432b4114c8ed7cdb
Linus Heck <linus.heck@rwth-aachen.de>
2023-08-19 10:37:26 +0200

    fix scalar diffusion benchmark

--- slurm/energybalance/betas_scalar_diffusion.sh
+++ slurm/energybalance/betas_scalar_diffusion.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-fixed_args="multistablesde/latent_sde.py --latent-size=1 --lr_gamma=0.9995 --model=energy --scalar-diffusion=True"
+fixed_args="multistablesde/latent_sde.py --latent-size=1 --lr_gamma=0.9995 --model=energy --kl-anneal-iters=0 --data-noise-level=0.135 --num-iters 10000 --scalar-diffusion=True"
 
 variable_args=(
 	"--beta=1"
@@ -7,7 +7,8 @@ variable_args=(
 	"--beta=100"
 	"--beta=1000"
 	"--beta=10000"
+	"--beta=100000"
+	"--beta=1000000"
 )
 
 srun python3 -m pipenv run python $fixed_args ${variable_args[$SLURM_ARRAY_TASK_ID]} --train-dir $1
-
