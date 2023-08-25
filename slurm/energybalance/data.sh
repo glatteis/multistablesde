6203f26a3c47ea7b5a1ed280dba43e738e494b02
Linus Heck <linus.heck@rwth-aachen.de>
2023-08-25 15:34:30 +0200

    modify batchsize experiment

--- slurm/energybalance/data.sh
+++ slurm/energybalance/data.sh
@@ -2,12 +2,11 @@
 fixed_args="multistablesde/latent_sde.py --latent-size=1 --lr_gamma=0.9995 --model=energy --kl-anneal-iters=1000 --data-noise-level=0.135 --num-iters 5000 --beta=10"
 
 variable_args=(
+    "--batch-size=4"
+    "--batch-size=16"
     "--batch-size=64"
-    "--batch-size=128"
     "--batch-size=256"
-    "--batch-size=512"
     "--batch-size=1024"
-    "--batch-size=2048"
     "--batch-size=4096"
 )
 
