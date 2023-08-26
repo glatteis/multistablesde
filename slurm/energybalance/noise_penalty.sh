9a72657a97fcefdc86b75b1482ee80e149d82432
wx133755 <wx133755@login18-1.hpc.itc.rwth-aachen.de>
2023-08-26 10:12:22 +0200

    noise penalty params

--- slurm/energybalance/noise_penalty.sh
+++ slurm/energybalance/noise_penalty.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-fixed_args="multistablesde/latent_sde.py --latent-size=1 --lr_gamma=0.9995 --model=energy --kl-anneal-iters=1000 --data-noise-level=0.135 --num-iters 5000 --beta=10"
+fixed_args="multistablesde/latent_sde.py --latent-size=1 --lr_gamma=0.9995 --model=energy --kl-anneal-iters=1000 --data-noise-level=0.135 --num-iters 10000 --beta=10"
 
 variable_args=(
-    "--noise-penalty=150"
+    "--noise-penalty=25"
+    "--noise-penalty=50"
+    "--noise-penalty=75"
+    "--noise-penalty=100"
     "--noise-penalty=200"
+    "--noise-penalty=400"
 )
 
 srun python3 -m pipenv run python $fixed_args ${variable_args[$SLURM_ARRAY_TASK_ID]} --train-dir $1
