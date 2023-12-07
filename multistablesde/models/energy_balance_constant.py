e62abf97327ddf90e66d11be866a298830c0cfbb
Linus Heck <linus.heck@rwth-aachen.de>
2023-12-07 16:17:45 +0100

    constant noise

--- multistablesde/models/energy_balance_constant.py
+++ multistablesde/models/energy_balance_constant.py
@@ -36,11 +36,9 @@ class ConstantStochasticEnergyBalance(object):
         x0 = ((torch.randn(batch_size, 1) * 20.0) + 270.0).to(device)
         """Sample data for training. Store data normalization constants if necessary."""
         xs = torchsde.sdeint(self, x0, ts, bm=bm)
-        if normalize:
-            mean, std = torch.mean(xs[0, :, :], dim=(0, 1)), torch.std(
-                xs[0, :, :], dim=(0, 1)
-            )
-            xs.sub_(mean).div_(std)
-            return xs, mean, std
-        else:
-            return xs
+        # Hack: don't check normalize, we always normalize - sorry
+        mean, std = torch.mean(xs[0, :, :], dim=(0, 1)), torch.std(
+            xs[0, :, :], dim=(0, 1)
+        )
+        xs.sub_(mean).div_(std)
+        return xs, mean, std
